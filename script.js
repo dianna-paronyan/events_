5cb7138ee96fdd9066e5b51adbf7effea6123e04
dianna1234 <01234dianna@gmail.com>
2022-01-21 23:34:01 +0400

    fourth commit

---
+++ script.js
@@ -0,0 +1,3 @@
+const ol = document.querySelector("ol");
+
+let clientEvents = ["click","resize", "load", "scroll", "mouseenter", "keydown", "keyup","keypress","focus", "mouseover","mouseout", "mouseup", "select", "change"];
