81756e1b662c2a2fc4cc340948d3e6b346e35ef6
dianna1234 <01234dianna@gmail.com>
2022-01-21 23:35:20 +0400

    fifth commit

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const ol = document.querySelector("ol");
 
 let clientEvents = ["click","resize", "load", "scroll", "mouseenter", "keydown", "keyup","keypress","focus", "mouseover","mouseout", "mouseup", "select", "change"];
+
+function clientEvent(eventType){
+    eventType.map((el)=>{
+        if(el){
+            window.addEventListener(el, ()=>{
+                eventType.push(el);
+                let li = document.createElement("li");
+                li.innerText += el;
+                ol.appendChild(li);
+            })
+        }
+    })
+}
+
+clientEvent(clientEvents);
