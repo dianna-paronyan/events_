0e19da87591836d9350d612bcfec4d94798ba80b
dianna1234 <01234dianna@gmail.com>
2022-01-24 21:12:40 +0400

    sixth commit

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-const ol = document.querySelector("ol");
+const olev = document.querySelector("#events");
+const olcode = document.querySelector("#codes");
 
 let clientEvents = ["click","resize", "load", "scroll", "mouseenter", "keydown", "keyup","keypress","focus", "mouseover","mouseout", "mouseup", "select", "change"];
+
+// function clientEvent(eventType){
+//     eventType.map((el)=>{
+//         if(el){
+//             window.addEventListener(el, ()=>{
+//                 eventType.push(el);
+//                 let li = document.createElement("li");
+//                 li.innerText += el;
+//                 ol.appendChild(li);
+//             })
+//         }
+//     })
+// }
 
 function clientEvent(eventType){
     eventType.map((el)=>{
         if(el){
             window.addEventListener(el, ()=>{
-                eventType.push(el);
+                // eventType.push(el);
                 let li = document.createElement("li");
                 li.innerText += el;
-                ol.appendChild(li);
+                olev.appendChild(li);
             })
+        }
+         if(el === "keyup"){
+            window.addEventListener(el, (e)=>{
+                // eventType.push(el);
+                let li = document.createElement("li");
+                li.innerText += e.code;
+                olcode.appendChild(li);
+            })    
         }
     })
 }
 
+
+
 clientEvent(clientEvents);
+
+// window.addEventListener("keydown",(e)=>{
+//     console.log(e.code)
+// })
